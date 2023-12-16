1ca68d10b52aaeeccba0c9729b35f475c7e46d31
Salman Mazhar <smazhar@gmail.com>
2023-12-16 21:38:23 +0600

    Changed plot

--- likert_plot.R
+++ likert_plot.R
@@ -1,16 +1,11 @@
 library(tidyverse)
 library(googlesheets4)
-library(sjPlot)
 library(likert)
 library(dplyr)
-library(lme4)        # for mixed-effects models
-library(lmerTest)    # for p-values in mixed-effects models
 library(knitr)
-library(ltm)
-library(gridExtra)
 library(ggplot2)
-library(reshape2)
 library(janitor) # Load the janitor package
+library(cowplot) # Load the cowplot package
 
 gs4_auth()
 
@@ -30,11 +25,31 @@ survey<- lapply(survey_data[, 3:32], function(x) factor(x, levels = levels))
 
 # Convert the list to a data frame
 survey_df <- as.data.frame(survey)
+names(survey_df) <- gsub("\\.", " ", names(survey_df))
 
 # Create a Likert plot
 likert_plot <- likert(survey_df)
 
-# Plot the Likert plot
-plot(likert_plot, wrap = 60) +
-  theme(plot.title = element_text(size = 20),
-        axis.title = element_text(size = 16))
+# Set the size of the output image
+width <- 15  # in inches, adjust as needed
+height <- 10  # in inches
+
+# Set the resolution of the output image
+dpi <- 600  # dots per inch
+
+# Set the size of the text in the plot
+base_size <- 10
+
+# Create the plot
+p <- plot(likert_plot, wrap = 60) +
+  theme(plot.title = element_text(size = base_size * 1.5, color = "black"),
+        axis.title = element_text(size = base_size, color = "black"),
+        axis.text = element_text(color = "black", size = base_size),
+        plot.margin = margin(5, 5, 5, 5, "mm"), # Adjust as needed
+        panel.border = element_rect(color = "black", fill = NA, linewidth = 1)) # Adjust linewidth as needed
+
+# Save the plot to a file with the specified size and resolution
+ggsave("likert_plot.png", plot = p, width = width, height = height, dpi = dpi)
+
+# Display the plot
+plot_grid(p, labels = "AUTO", ncol = 1)
